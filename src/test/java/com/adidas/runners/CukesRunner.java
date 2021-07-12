257b4ffe80a72b722acb6480ba0607eb179ca79b
Zgurskiy11 <zgurskiy11@gmail.com>
2021-07-12 10:14:36 -0600

    Initial commit

---
+++ src/test/java/com/adidas/runners/CukesRunner.java
@@ -0,0 +1,17 @@
+package com.adidas.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)// Cukes runner java class. Finding Paths to features and StepDef
+@CucumberOptions(
+        plugin = "html:target/cucumber-report.html",  // allows us to generate reports
+        features = "src/test/resources/features",//not same directory. So we coming from source level
+        glue = "com/cybertek/step_definitions", //step definitions path. Runner and StepDef are on the same level
+        dryRun = false,       //just run to see snippets. dryRun Allows us not to run actual implementation
+        tags = "@librarian or @student"        // uses features path. line 10
+)
+public class CukesRunner {
+
+}
